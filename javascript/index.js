da45f3a745e824256e5bdb725c61e7e650d51174
Matias Acuña <matiacu@gmail.com>
2022-06-02 17:24:59 -0300

    javascript ciclo

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+let entrada = prompt ('Ingresa tu contraseña')
+while (entrada != 'Hola123') { switch (entrada) {
+case "Hola124": alert ("Casi") 
+break;
+case "Hola1234": alert ("Contraseña Vieja")
+break;
+default: 
+alert ("Contraseña Incorrecta") }
+entrada = prompt ("Ingresar un nombre") }

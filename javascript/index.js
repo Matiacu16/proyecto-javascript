94f77a85c5320e29c9b9639a48ed51783d4c6ba5
Matias Acuña <matiacu@gmail.com>
2022-06-23 21:32:31 -0300

    Avanze Shop tienda java

--- javascript/index.js
+++ javascript/index.js
@@ -1,43 +1,107 @@
 //BODY APARTADO TIENDA
-// console.log(document)
 
-console.dir(document)
+//PRACTICA CONSOLA Y VARIABLES TIENDA
 
-//TITULO STYLE
+// console.log('addToShoppingCartButtons', addToShoppingCartButtons)
+// console.log('addToCartClicked -> button', button);
+// console.log('addToCartClicked -> item', item);
 
-let titulo = document.getElementById('title')
+//FUNCION DE FUSION DE PRUEBA
 
-const title = document.getElementsByClassName(titulo)
+// console.log('addToCartClicked -> itemTitle', itemTitle, itemPrice, itemImagen);
 
-for (const elemento of title) {
-    elemento.className = "black"
-    console.log(elemento)
+// VALORES CORRECTOS DE LOS ITEMS
+// console.log(itemTitle, itemPrice, itemImagen);
+
+//CONSOLA DEL SHOPPING PRECIO
+//console.log('updateShopingCartTotal -> shoppingCartTotal', shoppingCartTotal);
+//console.log('updateShoppingCartTotal -> shoppingCartItems', shoppingCartItems);
+//console.log('updateShoppingCartTotal -> shoppingCartItemPrice',shoppingCartItemPrice);
+
+const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
+addToShoppingCartButtons.forEach(addToCartButton => {
+    addToCartButton.addEventListener('click', addToCartClicked);
+});
+
+//VARIABLE GLOBAL SHOPPING
+const shoppingCartItemsContainer = document.querySelector('.shoppingCartItemsContainer');
+
+
+function addToCartClicked(event) {
+    const button = event.target;
+    const item = button.closest('.item');
+
+    const itemTitle = item.querySelector('.item-title').textContent;
+    const itemPrice = item.querySelector('.item-price').textContent;
+    const itemImagen = item.querySelector('.item-imagen').src;
+    
+addItemToShoppingCart(itemTitle, itemPrice, itemImagen);
 }
 
-const lista = document.getElementsByClassName('item_title');
+//INTERACCION CON HTML APARTADO SHOPPING CART
 
-//console.dir(lista)
-
-for (const elemento of lista) {
-    console.log(elemento.innerHTML)
+function addItemToShoppingCart(itemTitle, itemPrice, itemImagen){
+    const shoppingCartRow = document.createElement('div');
+    const shoppingCartContent = `
+    <div class="row shoppingCartItem">
+            <div class="col-6">
+                <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                    <img src=${itemImagen} class="shopping-cart-image">
+                    <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}}</h6>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                    <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div
+                    class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                    <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                    value="1">
+                <button class="btn btn-danger buttonDelete" type="button">X</button>
+            </div>
+        </div>
+    </div>`;
+    
+    //CONSOLA DEL AUTO SELECCIONADO 
+    console.log('Auto Seleccionado', itemTitle, itemPrice, itemImagen)
+    
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(shoppingCartRow);
+    
+    shoppingCartRow.querySelector('.buttonDelete').addEventListener('click', () => console.log('clicked'))
+    
+    
+    updateShoppingCartTotal();
 }
 
-//Todos Los H3
-let elemento = document.querySelectorAll('#contenedor h3')
+//FUNCION DE UPDATE DEL CARRO DE COMPRAS
+function updateShoppingCartTotal(){
+    let total = 0;
+    const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+    
+    const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+    
+    shoppingCartItems.forEach(shoppingCartItem => {
+    const shoppingCartItemPriceElement = shoppingCartItem.querySelector('.shoppingCartItemPrice');
+    
+    const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('U$S', ''));
+    
+    const shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity');
+    
+    const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
+    
+    total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+    
+    console.log('updateShoppingCartTotal -> shoppingCartItemPrice',shoppingCartItemPrice.toFixed(3));
+    
+    });
+    
+    shoppingCartTotal.innerHTML = `${total.toFixed(3)}U$S`
+}
 
-console.log(elemento)
 
-//Lista Automoviles
-
-const objetos = [
-    {Nombre:"Mercedes-Benz Clase A KIT AMG",Precio:"U$S 56990"},
-    {Nombre:"Audi A3 Full",Precio:"U$S 32000"},
-    {Nombre:"Audi A1 1.4",Precio:"U$S 26000"},
-    {Nombre:"Volkswagen Nivus Highline",Precio:"U$S 26990"},
-    {Nombre:"Volkswagen Gol Highline",Precio:"U$S 21500"},
-    {Nombre:"Renault Oroch 2022",Precio:"U$S 20990"},
-]
-
-objetos.map((value)=>{
-    console.log(value)
-})
+//CREAR UN REMOVE PARA EL SHOPPING CART ITEM!! TRABAJAR EN EL BOTON DE COMPRA PENDIENTE
+//EMPEZAR A CREAR PAG DE CONTACTO 

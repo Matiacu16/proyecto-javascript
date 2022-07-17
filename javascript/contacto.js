4491f35540723d36d01a36fc37c5a4971b4afcca
Matias Acuña <matiacu@gmail.com>
2022-07-17 20:22:09 -0300

    navbar arreglo

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -10,3 +10,60 @@ const showAlert = () => {
 
 let button = document.getElementById('enviarAlert');
 button.onclick = showAlert
+
+
+// const getDataByAJAX = () => {
+//     $.ajax({
+//         url: 'https://swapi.dev/api/people',
+//         success: function (result) {
+//             console.log('result: ', result);
+//         },
+//     });
+// }
+
+const fetchData = async (prams) => {
+    return new Promise ((resolve, reject) => {
+        try {
+            const options = {
+                method: params.method,
+                Headers: {
+                    accept: 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }
+        if (params.body) {
+            options.body = JSON.stringify(params.body)
+        }
+        let url = params.url;
+        if (params.query) {
+            url += '?' + Object.keys(params.query).map((k) => {
+                if (params.query[k] == '') {
+                    return;
+                }
+                return k + '=' + params.query[k];
+            }).join('&');
+        }
+        fetch(url, options)
+        .then((response) => response.json())
+        .then((resp) => {
+            if (resp.error && (typeof resp.status == 'number') && Math.floor(resp.status / 100) !=2) {
+                reject(resp);
+                return;
+            }
+            console.log('resp: ', resp);
+            resolve(resp);
+        }).catch((err) => {
+            reject(err);
+        });
+        } catch (err) {
+            reject(err);
+        }
+    })
+}
+
+fetchData(
+    {
+        method: 'GET',
+        url: 'https://swapi.dev/api/people',
+    }
+)
